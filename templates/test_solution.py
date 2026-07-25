cd9f0eb17717e4cf0f252d8795d78eb2285ea990
BahadirAydin <bahadiraydin1923@gmail.com>
2026-07-25 23:05:32 +0300

    Update template

--- templates/test_solution.py
+++ templates/test_solution.py
@@ -2,6 +2,4 @@ from solution import Solution
 
 
 def test_example_1():
-    # TODO: replace with a real example from the problem statement
-    sol = Solution()
-    assert sol.{{func}}() is None
+    assert Solution().{{func}}() is None
